bed70b2832c035c11614ad742931942fc16bed7a
kudohamu <kudohamu91086@gmail.com>
2017-03-30 18:30:46 +0900

    test(metadata): add test of ReadMetaData

---
+++ metadata_test.go
@@ -0,0 +1,41 @@
+package spirali
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetaData(t *testing.T) {
+	t.Run("ReadMetaData", func(t *testing.T) {
+		t.Run("when valid content", func(t *testing.T) {
+			content := `
+      {
+        "migrations": [
+           { "version": 20170102000000, "name": "create_foo_table" },
+           { "version": 20170101000000, "name": "create_bar_table" },
+           { "version": 20180101000000, "name": "create_baz_table" }
+        ]
+      }
+      `
+
+			m, err := ReadMetaData(bytes.NewReader([]byte(content)))
+			assert.NoError(t, err)
+			assert.Equal(t, uint64(20170101000000), m.Migrations[0].Version)
+			assert.Equal(t, "create_bar_table", m.Migrations[0].Name)
+			assert.Equal(t, uint64(20170102000000), m.Migrations[1].Version)
+			assert.Equal(t, "create_foo_table", m.Migrations[1].Name)
+			assert.Equal(t, uint64(20180101000000), m.Migrations[2].Version)
+			assert.Equal(t, "create_baz_table", m.Migrations[2].Name)
+		})
+
+		t.Run("when invalid content", func(t *testing.T) {
+			content := `migrations = []`
+
+			m, err := ReadMetaData(bytes.NewReader([]byte(content)))
+			assert.Error(t, err)
+			assert.Nil(t, m)
+		})
+	})
+}
